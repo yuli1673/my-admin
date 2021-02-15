25d1d4a55884fa2415649fc50e4fea1d2725bba7
josen <yuli19950403@aliyun.com>
2021-02-15 21:08:29 +0800

    add docs

---
+++ docs/precache-manifest.c25b0be705b50cf1cc98bc9d767db1ea.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d5081b13da99464f4d",
+    "url": "/css/app.5c657342.css"
+  },
+  {
+    "revision": "b449528dc71e46ba5678",
+    "url": "/css/chunk-5e6019d6.bb0e5a4b.css"
+  },
+  {
+    "revision": "433a9cd89133ea3264f9",
+    "url": "/css/chunk-7ce9d0be.0fcccab2.css"
+  },
+  {
+    "revision": "22d892c250fab3b38250",
+    "url": "/css/chunk-d830a6b0.05386ca4.css"
+  },
+  {
+    "revision": "e32c2c91e8811d4d980b",
+    "url": "/css/chunk-d9890892.2c1fd3c4.css"
+  },
+  {
+    "revision": "02e6a8451d8764f2b4d7",
+    "url": "/css/chunk-vendors.c470e980.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "848b3d90886ade164df43a6e91a7ccac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d5081b13da99464f4d",
+    "url": "/js/app.31e9de1d.js"
+  },
+  {
+    "revision": "0a21b48005b507e65216",
+    "url": "/js/chunk-2d0af8f8.76fed757.js"
+  },
+  {
+    "revision": "564c13b03a847fed50de",
+    "url": "/js/chunk-2d0ceb84.46f30a95.js"
+  },
+  {
+    "revision": "37501bf31ab60932c5ba",
+    "url": "/js/chunk-2d0e95df.68b0ae31.js"
+  },
+  {
+    "revision": "40c8afcc53e93c5fea65",
+    "url": "/js/chunk-2d224560.1d7ce76f.js"
+  },
+  {
+    "revision": "b449528dc71e46ba5678",
+    "url": "/js/chunk-5e6019d6.28c83e5d.js"
+  },
+  {
+    "revision": "f3e5606c5228ac52ad0d",
+    "url": "/js/chunk-712f2df5.fb825d6c.js"
+  },
+  {
+    "revision": "433a9cd89133ea3264f9",
+    "url": "/js/chunk-7ce9d0be.fd246fff.js"
+  },
+  {
+    "revision": "22d892c250fab3b38250",
+    "url": "/js/chunk-d830a6b0.f8337f45.js"
+  },
+  {
+    "revision": "e32c2c91e8811d4d980b",
+    "url": "/js/chunk-d9890892.59d3e051.js"
+  },
+  {
+    "revision": "02e6a8451d8764f2b4d7",
+    "url": "/js/chunk-vendors.189889fe.js"
+  },
+  {
+    "revision": "8978a31e3acbb87d9f9b4cd43aca123f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
